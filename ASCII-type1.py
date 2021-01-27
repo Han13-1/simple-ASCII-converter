b84d9ec370c2826e21b0637845c45f9d101c9a89
Han13-1 <74622232+Han13-1@users.noreply.github.com>
2021-01-27 15:27:13 +0100

    Update ASCII-type1.py

--- ASCII-type1.py
+++ ASCII-type1.py
@@ -2,12 +2,19 @@ print("if you want to get the letter of your value type : letter\nif you want to
 
 z = str(input(""))
 
-if z == "letter": # getting ASCII value from user
-    while (1==0) :
+if z == "letter":
+
+ # getting ASCII value from user
+
+    i=0
+    while i<1 :
         inp = input("Enter ASCII value: ")
         num = int(inp)
         print(chr(num))
-if z == str("value"): # get ASCII value
+if z == str("value"): 
+
+    # get ASCII value
+
     count = 0
     while count  <1:
         Letter = input("Enter your character: ")
