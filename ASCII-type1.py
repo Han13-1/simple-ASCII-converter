387e7322b2e8daea091172ed5c4b064c2bd0a10b
Han13-1 <74622232+Han13-1@users.noreply.github.com>
2021-01-27 14:32:03 +0100

    Add files via upload

---
+++ ASCII-type1.py
@@ -0,0 +1,14 @@
+print("if you want to get the letter of your value type : letter\nif you want to get a value from your letter type : value")
+
+z = str(input(""))
+
+if z == "letter": # getting ASCII value from user
+    while (1==0) :
+        inp = input("Enter ASCII value: ")
+        num = int(inp)
+        print(chr(num))
+if z == str("value"): # get ASCII value
+    count = 0
+    while count  <1:
+        Letter = input("Enter your character: ")
+        print(ord(Letter))
